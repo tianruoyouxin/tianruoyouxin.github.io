bf009b030f967e0d29845a35a979f68fb9d5f77a
tianruo <43494597+tianruoyouxin@users.noreply.github.com>
2018-09-30 15:27:35 +0800

    Update sm.js

--- page/js/sm.js
+++ page/js/sm.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var options = {
       beforeSubmit: showRequest, //提交前的回调函数
       success: showResponse, //提交后的回调函数
+      clearForm: true,
       timeout: 5000 //限制请求的时间，当请求大于3秒后，跳出请求
     };
     function showRequest(formData, jqForm, options) {
@@ -36,3 +37,4 @@ $(document).ready(function() {
     $("#btn").click();
   });
 });
+clearForm: true,
